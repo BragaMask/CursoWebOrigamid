44d56e4844d0cb96ef2f4ab0292cc2755e4cccb0
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-01-07 14:02:55 -0400

    Aprendendo sobre as variaveis

---
+++ JavaScriptBasico/script.js
@@ -0,0 +1,6 @@
+console.log("Olá, mundo!");
+
+//Variaveis
+let nome = "João";  // A variável pode ser modificada
+const idade = 25; // A variável não pode ser modificada
+var cidade = "São Paulo"; // Variável com escopo global (um coringa)
