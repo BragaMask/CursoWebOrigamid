50b69cf3cd1135bfb6a5c7f43de07834f63894cd
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-01-16 20:54:29 -0400

    Finalizando os estudos em Posicionamento + Uma parte do exercicio sobre

--- JavaScriptBasico/script.js
+++ JavaScriptBasico/script.js
@@ -1,6 +1,19 @@
-console.log("Olá, mundo!");
 
 //Variaveis
+/*
 let nome = "João";  // A variável pode ser modificada
 const idade = 25; // A variável não pode ser modificada
 var cidade = "São Paulo"; // Variável com escopo global (um coringa)
+*/
+
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+
+console.log(nav);
+console.log(produtos);
+
+console.log(produtos.href);
+
+nav.style.backgroundColor = "cyan";
+produtos.style.color = "red";
+nav.style.padding = "10px";
