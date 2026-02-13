7b7e7959c290ef446a703e799dd3f9076e044559
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-12 22:30:39 -0400

    Aprendendo Variaveis em JavaScript

---
+++ javaScriptCompleto/variaveis/script.js
@@ -0,0 +1,16 @@
+//var, let, const
+
+var nomeTeste = "Arthur";
+let idadeTeste = 17;
+const estudaNoIfroTeste = true;
+
+console.log(nomeTeste,idadeTeste,estudaNoIfroTeste);
+
+var semDefinir; //undefined
+
+//Exercicio
+let nome = "Arthur";
+let idade = 17;
+let comidaFavorita;
+comidaFavorita = "Espaguete com molho bolonhesa";
+var a,b,c,d,e;
