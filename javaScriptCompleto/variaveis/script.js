badb13d1a3139a777064df601b9e461113d475c1
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-12 22:42:29 -0400

    Exercicio básico sobre tipos de elementos

--- javaScriptCompleto/variaveis/script.js
+++ javaScriptCompleto/variaveis/script.js
@@ -8,9 +8,22 @@ console.log(nomeTeste,idadeTeste,estudaNoIfroTeste);
 
 var semDefinir; //undefined
 
-//Exercicio
+//Exercicio - variaveis
 let nome = "Arthur";
 let idade = 17;
 let comidaFavorita;
 comidaFavorita = "Espaguete com molho bolonhesa";
 var a,b,c,d,e;
+
+//Exercicio - Tipos de Dados
+var palavra = "Barco";
+
+var numPalavra = "10";
+
+var idadeDnv = 17;
+
+var nomeDnv = "Arthur";
+var sobrenome = "Rodrigues Braga";
+var nomeCompleto = `${nomeDnv} ${sobrenome}`;
+
+console.log(nomeCompleto);
