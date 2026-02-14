8cb98d612f59b00db4e640a4ec8ab9a82f47960e
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-14 12:08:36 -0400

    Aprendendo sobre DOM

---
+++ javaScriptCompleto/dom/dom-para-iniciantes/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+const URL = window.location.href;
+console.log(URL);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const selecionarClass = document.querySelector(".ativo");
+console.log(selecionarClass);
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language
+console.log(linguagem)
+
+// Retorne a largura da janela 
+const largura = window.outerWidth;
+console.log(largura);
