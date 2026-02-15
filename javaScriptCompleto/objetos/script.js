460e6863dd8a3b8b5738c2d79bfcd8c56f048f4b
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-14 23:09:22 -0400

    fazendo exercicios de objetos no JS

--- javaScriptCompleto/objetos/script.js
+++ javaScriptCompleto/objetos/script.js
@@ -1,7 +1,17 @@
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
+var dadosPessoais = {
+  nome: "Arthur",
+  sobrenome: "Rodrigues Braga",
+  idade: 17,
+  nomeCompleto: function(nome, sobrenome) {
+    return nome + "" + sobrenome;
+  }
+}
+
 
 // Crie um método no objeto anterior, que mostre o seu nome completo
+
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
@@ -10,6 +20,22 @@ var carro = {
   marca: 'Audi',
 }
 
+carro.preco = 3000;
+
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
 
+var cachorro = {
+  raca: "labrador",
+  cor: "preto",
+  idade: 10,
+  verHomem: true,
+  latir: function(verHomem) {
+    if (verHomem == true) {
+      return "au au"
+    }
+    else{
+      return "*silencio*"
+    }
+  }
+}
