0f65afc877eba089e07e78efc3e831a46e50d515
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-23 13:30:27 -0400

    Aprendendo sobre Classes e atributos

--- javaScriptCompleto/dom/selecionar-dom/script.js
+++ javaScriptCompleto/dom/selecionar-dom/script.js
@@ -1,22 +1,18 @@
-// Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll('p');
-paragrafos.forEach((item) => {
-        console.log(item)
-    });
-// Mostre o texto dos parágrafos no console
-paragrafos.forEach((item) => 
-    {console.log(item.innerText)})
-// Como corrigir os erros abaixo:
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach((item) => {
+    item.classList.add('ativo');
+});
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item) => {
+    item.classList.remove('ativo');
+});
+itensMenu[0].classList.add('ativo');
+// Verifique se as imagens possuem o atributo alt
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach(item, index => {
-    console.log(item, index);
-});
-
-let i = 0;
-imgs.forEach(() => {
-    console.log(i++)
-}
-);
-
-imgs.forEach(() => i++);
+imgs.forEach((img) => {
+    const possuiAtributo = img.hasAttribute('alt');
+    console.log(possuiAtributo);
+})
