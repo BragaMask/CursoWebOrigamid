9bfcb8a145b5c27af1ca3b456cc650aad3453ae0
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-23 17:10:16 -0400

    Aprendendo sobre eventos no JavaScript

--- javaScriptCompleto/dom/selecionar-dom/script.js
+++ javaScriptCompleto/dom/selecionar-dom/script.js
@@ -1,42 +1,24 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const imgs = document.querySelector('img');
-console.log(imgs.offsetTop);
+const img = document.querySelector('img');
 
-// Retorne a soma da largura de todas as imagens
-function LarguraTotal() {
-    let soma = 0;
-    const imagens = document.querySelectorAll('img');
-    imagens.forEach((imagem) => {
-        soma = soma + imagem.offsetWidth;
-    })
-    console.log(soma);
+function clicou(event) {
+    console.log(event);
+}
+//img.addEventListener('click', clicou)
+
+const imagensLista = document.querySelector('.animais-lista');
+
+function callBackLista(event) {
+    console.log(event.currentTarget);
+    console.log(event.target);
 }
 
-window.onload = function() {
-    LarguraTotal();
+imagensLista.addEventListener('click', callBackLista)
+
+const linkExterno = document.querySelector('a[href^="http"]')
+
+function handleLinkExterno(event){
+    event.preventDefault();
+    console.log('Clicou');
 }
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('a');
-
-links.forEach((link) => {
-    linkWidth = link.offsetWidth;
-    linkHeight = link.offsetHeight;
-
-    if (linkWidth >= 48 && linkHeight >= 48) {
-        console.log(link, 'Possui boa acessibilidade');
-    } else {
-        console.log(link, 'Não possui boa acessibilidade');
-    }
-})
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const tamanho = window.matchMedia('(max-width: 720px)').matches;
-
-if(tamanho){
-    const menu = document.querySelector('.menu');
-    menu.classList.add('menu-mobile')
-}
+linkExterno.addEventListener('click', handleLinkExterno)
