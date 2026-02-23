5a94a8081696726c53a2f620e1f741bb8797bb5b
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-02-23 14:57:44 -0400

    Realizando Exercicios

--- javaScriptCompleto/dom/selecionar-dom/script.js
+++ javaScriptCompleto/dom/selecionar-dom/script.js
@@ -1,28 +1,42 @@
-const listaAnimais = document.querySelector('.animais-lista');
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const imgs = document.querySelector('img');
+console.log(imgs.offsetTop);
 
-const heightTotal = listaAnimais.scrollHeight;
-console.log(heightTotal);
-
-//listaAnimais.clientHeight; - height + padding
-//listaAnimais.offsetHeight; - height + padding + border
-//listaAnimais.scrollHeight; - height total, mesmo dentro de scroll
-
-const distanciaTopo = listaAnimais.offsetTop;
-console.log(distanciaTopo);
-
-const rect = listaAnimais.getBoundingClientRect();
-console.log(rect.width);
-//rect.height - tamanho do Height do elemento
-//rect.width - tamanho do width do elemento
-
-console.log(window.innerWidth);
-
-const small = window.matchMedia('(max-width: 600px)').matches;
-
-if (small) {
-    console.log('usuario mobile')
-} else {
-    console.log('usuario desktop')
+// Retorne a soma da largura de todas as imagens
+function LarguraTotal() {
+    let soma = 0;
+    const imagens = document.querySelectorAll('img');
+    imagens.forEach((imagem) => {
+        soma = soma + imagem.offsetWidth;
+    })
+    console.log(soma);
 }
 
-console.log(small)
+window.onload = function() {
+    LarguraTotal();
+}
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+
+links.forEach((link) => {
+    linkWidth = link.offsetWidth;
+    linkHeight = link.offsetHeight;
+
+    if (linkWidth >= 48 && linkHeight >= 48) {
+        console.log(link, 'Possui boa acessibilidade');
+    } else {
+        console.log(link, 'Não possui boa acessibilidade');
+    }
+})
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+const tamanho = window.matchMedia('(max-width: 720px)').matches;
+
+if(tamanho){
+    const menu = document.querySelector('.menu');
+    menu.classList.add('menu-mobile')
+}
