cf109ef8c4f59583f74db08a3ab404b4a442dd97
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-04-25 22:01:54 -0400

    exercicios prototype

--- javaScriptCompleto/objetos/script.js
+++ javaScriptCompleto/objetos/script.js
@@ -1,22 +1,44 @@
 //prototype -> objeto adicionado a uma função quando a mesma é criada.
 
-function Pessoa(nome, sobrenome) {
-    this.nome = nome;
-    this.sobrenome = sobrenome;
+//Exercicios
+
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
 }
 
-Pessoa.prototype.morreu = function(){
-  console.log('acho que ele morreu');
+Pessoa.prototype.nomeCompleto = function(){
+  return `${this.nome} ${this.sobrenome}`;
 }
 
-const p1 = new Pessoa('Luiz', 'Otávio');
+const andrezinho = new Pessoa('Andrezinho', 'Silva', 30);
 
-console.log(Pessoa.prototype); //retorna o objeto 
-console.log(p1.prototype); //undefined
+console.log(andrezinho.nomeCompleto());
 
-console.log(p1.morreu());
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
 
-//construtores nativos - Objetos, Funções, Números, Strings, etc... possuem o prototype, ou seja, possuem métodos e propriedades.
+Object.getOwnPropertyNames(NodeList.prototype);
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
 
-const pais = 'Brasil'; 
-const cidade = new String('Rio'); //String é um construtor nativo, ou seja, tem o prototype.
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li; // HTMLLIElement
+li.click; // function
+li.innerText; // string
+li.value; // number
+li.hidden; // boolean
+li.offsetLeft; // number
+li.click(); // undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; //string porque está retornando o NOME DO CONSTRUTOR E O VALOR É UMA STRING
